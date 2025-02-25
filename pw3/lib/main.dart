73fe2d77acc75c2729869ca87484c0d6800e135a
Andrii Derevianko <59490330+DereviankoAndrew@users.noreply.github.com>
2025-02-25 06:50:17 +0100

    pw3

--- pw3/lib/main.dart
+++ pw3/lib/main.dart
@@ -1,122 +1,111 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    return MaterialApp(home: SolarCalculatorApp());
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class SolarCalculatorApp extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _SolarCalculatorAppState createState() => _SolarCalculatorAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _SolarCalculatorAppState extends State<SolarCalculatorApp> {
+  TextEditingController pcController = TextEditingController();
+  TextEditingController energyPriceController = TextEditingController();
+  TextEditingController sigmaController = TextEditingController();
+  String revenue = "";
+  String fine = "";
+  String profit = "";
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      appBar: AppBar(title: Text("Solar Calculator")),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                controller: pcController,
+                decoration: InputDecoration(labelText: "Середньодобова потужність, Pc (МВт)"),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                controller: energyPriceController,
+                decoration: InputDecoration(labelText: "Вартість електроенергії, В (грн/кВт*год)"),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+              SizedBox(height: 8),
+              TextField(
+                controller: sigmaController,
+                decoration: InputDecoration(labelText: "Cередньоквадратичне відхилення, σ"),
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  double powerAverage = double.tryParse(pcController.text) ?? 0.0;
+                  double price = double.tryParse(energyPriceController.text) ?? 0.0;
+                  double deviation = double.tryParse(sigmaController.text) ?? 0.0;
+                  double noImbalancesEnergyShare = integration(powerAverage, 5000, deviation);
+                  double _revenue = powerAverage * 24 * noImbalancesEnergyShare * price;
+                  double _fine = powerAverage * 24 * (1 - noImbalancesEnergyShare) * price;
+                  double _profit = _revenue - _fine;
+                  setState(() {
+                    revenue = _revenue.toString();
+                    fine = _fine.toString();
+                    profit = _profit.toString();
+                  });
+                },
+                child: Text("Обчислити"),
+              ),
+              SizedBox(height: 16),
+              Text("Дохід: " + revenue),
+              SizedBox(height: 8),
+              Text("Штраф: " + fine),
+              SizedBox(height: 8),
+              Text("Прибуток: " + profit),
+              SizedBox(height: 8),
+              Builder(builder: (context) {
+                double p = double.tryParse(profit) ?? 0.0;
+                String conclusion = p > 0 ? "Модель є рентабельна" : "Модель не є рентабельна";
+                return Text("Висновок: " + conclusion);
+              }),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+double normalPowerDistributionLaw(double p, double pC, double sigma1) {
+  double coefficient = 1 / (sigma1 * sqrt(2 * pi));
+  double exponent = -pow((p - pC), 2) / (2 * pow(sigma1, 2));
+  return coefficient * exp(exponent);
+}
+
+double integration(double powerAverage, int numSteps, double deviation) {
+  double lowerBound = powerAverage - powerAverage * 0.05;
+  double upperBound = powerAverage + powerAverage * 0.05;
+  double stepSize = (upperBound - lowerBound) / numSteps;
+  double res = 0.0;
+  for (int i = 0; i < numSteps; i++) {
+    double p1 = lowerBound + i * stepSize;
+    double p2 = p1 + stepSize;
+    double npdl1 = normalPowerDistributionLaw(p1, powerAverage, deviation);
+    double npdl2 = normalPowerDistributionLaw(p2, powerAverage, deviation);
+    res += 0.5 * (npdl1 + npdl2) * stepSize;
+  }
+  return res;
 }
